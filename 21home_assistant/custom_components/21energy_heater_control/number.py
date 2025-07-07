cb6fcaa0c4d5d98c8abe62e6801f54e947bc3e2e
21energy <165808173+21energy@users.noreply.github.com>
2025-07-07 10:00:37 +0200

    Delete 21home_assistant directory

--- 21home_assistant/custom_components/21energy_heater_control/number.py
+++
@@ -1,6 +0,0 @@
-# number.py (root)
-from .ofen.number import async_setup_entry as setup_ofen_numbers
-
-
-async def async_setup_entry(hass, entry, async_add_entities):
-    await setup_ofen_numbers(hass, entry, async_add_entities)
